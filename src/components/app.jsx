becd5f4dffbe2a113b9060d692a3699a207bcf9e
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-10-29 15:06:49 -0400

    giflist component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import SearchBar from "./search_bar.jsx";
 import Gif from "./gif.jsx";
+import GifList from "./gif_list.jsx";
 
 class App extends Component {
+
   render() {
+    const gifs = [
+      { id: '6utOeiLIAouyH9H5FS'},
+      { id: '6utOeiLIAouyH9H5FS'}
+    ];
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
-          <Gif />
+          <Gif id='6utOeiLIAouyH9H5FS' />
         </div>
         <div className="right-scene">
+          <div className="gif-list">
+            <GifList gifs={gifs} />
+          </div>
         </div>
       </div>
-      );
+    );
   }
 }
 
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`
     return (
         <div className="selected-gif">
-          <img src="https://media.giphy.com/media/6utOeiLIAouyH9H5FS/giphy.gif" alt="" className="gif"/>
+          <img src={src} alt="" className="gif"/>
         </div>
       );
   }
